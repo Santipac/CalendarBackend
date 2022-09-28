3774a69b4034375ac8b42ac5f546adacc4616faf
Santi <santiagopacini@gmail.com>
2022-09-28 13:19:37 -0300

    mongoDB fixed?

--- build/databases/config.js
+++ build/databases/config.js
@@ -1,28 +1,56 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+'use strict';
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const mongoose_1 = __importDefault(require('mongoose'));
 const MONGODB_URL = process.env.MONGODB_URL;
-const dbConnection = () => __awaiter(void 0, void 0, void 0, function* () {
+const dbConnection = () =>
+  __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose_1.default.connect(MONGODB_URL ||
-            'mongodb+srv://MERN_USER:HSayPC5Afal3p1jU@calendardb.uwhy6oc.mongodb.net/mern_calendar');
-        console.log('🚀 DataBase is running!');
+      yield mongoose_1.default.connect(
+        MONGODB_URL ||
+          'mongodb+srv://MERN_USER:HSayPC5Afal3p1jU@calendardb.uwhy6oc.mongodb.net/mern_calendar'
+      );
+      console.log('🚀 DataBase is running!');
+    } catch (error) {
+      console.log(error);
+      throw new Error('Could not connect to MongoDB. Please try again');
     }
-    catch (error) {
-        console.log(error);
-        throw new Error('Could not connect to MongoDB. Please try again');
-    }
-});
+  });
 exports.default = dbConnection;
--- src/databases/config.ts
+++ src/databases/config.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL as string;
-
 const dbConnection = async () => {
   try {
     await mongoose.connect(
-      MONGODB_URL ||
+      process.env.MONGODB_URL ||
         'mongodb+srv://MERN_USER:HSayPC5Afal3p1jU@calendardb.uwhy6oc.mongodb.net/mern_calendar'
     );
     console.log('🚀 DataBase is running!');
