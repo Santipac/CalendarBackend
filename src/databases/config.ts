231db81ab3d2db1c27251ab29cfddc15938a9dc4
Santi <santiagopacini@gmail.com>
2022-09-28 13:15:56 -0300

    MongoDB connection fixed

--- src/databases/config.ts
+++ src/databases/config.ts
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 
+const MONGODB_URL = process.env.MONGODB_URL as string;
+
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.TSC_DB_CNN as string);
+    await mongoose.connect(
+      MONGODB_URL ||
+        'mongodb+srv://MERN_USER:HSayPC5Afal3p1jU@calendardb.uwhy6oc.mongodb.net/mern_calendar'
+    );
     console.log('🚀 DataBase is running!');
   } catch (error) {
     console.log(error);
